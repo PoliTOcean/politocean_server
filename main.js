b1fe0f0982763653294c1b8aeea20e1f790e4258
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-12-03 11:17:05 +0100

    Removed mining app

--- main.js
+++ main.js
@@ -2,13 +2,10 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 
 import electronics from './electronics';
-import mining from './mining'
+// import mining from './mining'
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-}).then(async () => {
-    electronics.listen(process.env.ELEC_PORT, () => console.log(`Server listening on port ${process.env.ELEC_PORT}`));
-    mining.listen(process.env.MINING_PORT, () => console.log(`Server listening on port ${process.env.MINING_PORT}`));
-});
+}).then(() => electronics.listen(process.env.ELEC_PORT, () => console.log(`Server listening on port ${process.env.ELEC_PORT}`)));
