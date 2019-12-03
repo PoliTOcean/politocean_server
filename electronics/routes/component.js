d5fd697c605b9610d4a612d53dca32ed7117b741
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-12-03 13:20:13 +0100

    Updated components routing

--- electronics/routes/component.js
+++ electronics/routes/component.js
@@ -10,9 +10,22 @@ var getComponents = async (req, res) => {
     try {
         const results = await query;
         return res.status(200).send({ results: results });
-    } catch (_) {}
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+}
 
-    return res.status(400).send({ error: "Some error"});
+var newComponent = async (req, res) => {
+    try {
+        let component = new req.context.models.Component(req.body);
+        await component.validate();
+
+        const result = await component.save();
+
+        return res.status(200).send({ result: result });
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
 }
 
 var getComponentById = async (req, res) => {
@@ -25,12 +38,42 @@ var getComponentById = async (req, res) => {
     try {
         const result = await query;
         return res.status(200).send({ result: result });
-    } catch (_) {}
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+}
 
-    return res.status(400).send({ error: "Some error" });
+var updateComponentById = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send({ error: "Invalid component ID" });
+    }
+
+    const query = req.context.models.Component.update({ _id: req.params.id }, req.body);
+
+    try {
+        const result = await query;
+        return res.status(200).send({ result: result });
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+}
+
+var deleteComponentById = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send({ error: "Invalid component ID" });
+    }
+    
+    /**
+     * TODO: Check user's privilege.
+     * The main idea is to send as body the current user model to check if he has the right permissions
+     */
 }
 
 router.get('/', getComponents);
+router.post('/', newComponent);
+
 router.get('/:id', getComponentById);
+router.put('/:id', updateComponentById);
+router.delete('/:id', deleteComponentById);
 
 export default router;
