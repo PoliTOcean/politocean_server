436a46c209cf6a6e1b387ef3e449c19ef1caa69d
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-12-03 15:53:07 +0100

    Added User routes

---
+++ electronics/routes/user.js
@@ -0,0 +1,77 @@
+import { Router } from 'express';
+
+const router = Router();
+
+var getUsers = async (req, res) => {
+    const query = req.context.models.User.find();
+
+    try {
+        const results = await query;
+        return res.status(200).send({ results: results });
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+}
+
+var newUser = async (req, res) => {
+    try {
+        let user = new req.context.models.User(req.body);
+        await user.validate();
+
+        const result = await user.save();
+
+        return res.status(200).send({ result: result });
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+}
+
+var getUserById = async (req, res) => {
+    if (Number.isInteger(req.params.id)) {
+        return res.status(400).send({ error: "Invalid user ID" });
+    }
+
+    const query = req.context.models.User.findById(req.params.id);
+    
+    try {
+        const result = await query;
+        return res.status(200).send({ result: result });
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+}
+
+var updateUserById = async (req, res) => {
+    if (Number.isInteger(req.params.id)) {
+        return res.status(400).send({ error: "Invalid user ID" });
+    }
+
+    const query = req.context.models.User.update({ _id: req.params.id }, req.body);
+
+    try {
+        const result = await query;
+        return res.status(200).send({ result: result });
+    } catch (err) {
+        return res.status(400).send({ error: err });
+    }
+}
+
+var deleteUserById = async (req, res) => {
+    if (Number.isInteger(req.params.id)) {
+        return res.status(400).send({ error: "Invalid user ID" });
+    }
+    
+    /**
+     * TODO: Check user's privilege.
+     * The main idea is to send as body the current user model to check if he has the right permissions
+     */
+}
+
+router.get('/', getUsers);
+router.post('/', newUser);
+
+router.get('/:id', getUserById);
+router.put('/:id', updateUserById);
+router.delete('/:id', deleteUserById);
+
+export default router;
