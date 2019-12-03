4563d91c88e5f8b44c68fb0f8ddadad3d03b45ad
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-12-03 12:24:31 +0100

    Added search by ID

--- electronics/routes/component.js
+++ electronics/routes/component.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+var ObjectId = require('mongoose').Types.ObjectId;
+
 const router = Router();
 
 var getComponents = async (req, res) => {
@@ -13,6 +15,22 @@ var getComponents = async (req, res) => {
     return res.status(400).send({ error: "Some error"});
 }
 
+var getComponentById = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(400).send({ error: "Invalid component ID" });
+    }
+
+    const query = req.context.models.Component.findById(req.params.id);
+    
+    try {
+        const result = await query;
+        return res.status(200).send({ result: result });
+    } catch (_) {}
+
+    return res.status(400).send({ error: "Some error" });
+}
+
 router.get('/', getComponents);
+router.get('/:id', getComponentById);
 
 export default router;
--- electronics/routes/index.js
+++ electronics/routes/index.js
@@ -1,3 +1,3 @@
-import component from './component';
+import components from './component';
 
-export default { component };
+export default { components };
