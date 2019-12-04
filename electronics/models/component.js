c4e515226a4990cba75126880b3cc29ce655fa02
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2019-12-04 12:46:07 +0100

    Using population

--- electronics/models/component.js
+++ electronics/models/component.js
@@ -9,11 +9,21 @@ const componentSchema = new mongoose.Schema({
     quantity: Number,
     note: String,
     link: String,
-    created_by: Number,
+    created_by: {
+        type: mongoose.Schema.Types.Number,
+        ref: 'User',
+        autopopulate: true
+    },
     created_on: Date,
-    updated_by: Number,
+    updated_by: {
+        type: mongoose.Schema.Types.Number,
+        ref: 'User',
+        autopopulate: true
+    },
     updated_on: Date
 });
+
+componentSchema.plugin(require('mongoose-autopopulate'));
 
 const Component = mongoose.model('Component', componentSchema);
 
